130d9c134194d1d78dc50886119fd280b90ecf32
Gallyamov Vitaly <vitgvr@gmail.com>
2014-09-01 10:19:24 +0600

    add 3g mode for sending data

--- app/src/main/java/ru/amobilestudio/autorazborassistant/receivers/NetworkChangeReceiver.java
+++ app/src/main/java/ru/amobilestudio/autorazborassistant/receivers/NetworkChangeReceiver.java
@@ -8,7 +8,6 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 
 import ru.amobilestudio.autorazborassistant.asyncs.MainAsync;
-import ru.amobilestudio.autorazborassistant.helpers.ActivityHelper;
 
 public class NetworkChangeReceiver extends BroadcastReceiver {
 
@@ -27,10 +26,9 @@ public class NetworkChangeReceiver extends BroadcastReceiver {
             NetworkInfo netInfo = conMan.getActiveNetworkInfo();
 
             if (netInfo != null) {
-                if (netInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                if (netInfo.getType() == ConnectivityManager.TYPE_WIFI || netInfo.getType() == ConnectivityManager.TYPE_MOBILE) {
                     //start sync process
                     if(_mainAsync == null){
-                        ActivityHelper.debug("not create async");
                         _mainAsync = new MainAsync(context);
                         _mainAsync.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
                     }else if(_mainAsync.getStatus() == AsyncTask.Status.FINISHED){
@@ -39,12 +37,9 @@ public class NetworkChangeReceiver extends BroadcastReceiver {
                     }
                 }else{
                     cancelMainAsync();
-//                    AlertDialogHelper.showAlertDialog(context, R.string.alert_title, R.string.turn_on_wifi, true);
                 }
             }else{
                 cancelMainAsync();
-                //AlertDialogHelper.showAlertDialog(context, R.string.alert_title, R.string.turn_on_wifi, true);
-                ActivityHelper.debug("wifi no");
             }
         }
 
